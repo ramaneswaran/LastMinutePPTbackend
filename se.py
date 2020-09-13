9f9030b0ca3351abb623a8c44b9ddf0496d5712c
ramaneswaran <ramannewdelhi44@gmail.com>
2020-09-13 16:00:14 +0530

    Add selenium

---
+++ se.py
@@ -0,0 +1,8 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+driver = webdriver.Chrome()
+driver.get('http://christopher.su')
+print(driver.title)
